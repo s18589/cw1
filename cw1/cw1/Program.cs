d62ee0427353024831e4974669eb371efa5278c6
s18589 <s18589@pjwstk.edu.pl>
2020-03-05 11:35:06 +0100

    cw1 some update

--- cw1/cw1/Program.cs
+++ cw1/cw1/Program.cs
@@ -1,27 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace cw1
 {
+    public class Student
+    {
+        public string Imie { get; set; }
+
+        private string _nazwisko;
+
+        public string Nazwisko
+        {
+            get { return _nazwisko; }
+            set {_nazwisko = value; }
+        }
+
+    }
+
     public class Program
     {
         public static async Task Main(string[] args)
         {
-            var httpClient = new HttpClient();
-            var response = await httpClient.GetAsync("https://www.pja.edu.pl");
-
-            if (response.IsSuccessStatusCode)
+            if (args.Length == 0) return;
+            string url = args.Length > 0 ? args[0] : "https://www.pja.edu.pl";
+            try
             {
+                var httpClient = new HttpClient();
+                var response = await httpClient.GetAsync(url);
+
+                var list = new List<string>();
+                var zbior = new HashSet<string>();
+                var slownik = new Dictionary<string, int>();
+
+                if (!response.IsSuccessStatusCode) return;
+                
                 string html = await response.Content.ReadAsStringAsync();
                 var regex = new Regex("[a-z]+[a-z0-9]*@[a-z.]+");
                 var matches = regex.Matches(html);
 
-                foreach(var m in matches)
-                {
-                    Console.WriteLine(m);
-                }
+                    foreach (var m in matches)
+                    {
+                        Console.WriteLine(m);
+                    }
+                
+            }catch(Exception exc)
+            {
+                //string.Format("Wystąpił błąd {0}", exc.ToString());
+                Console.WriteLine($"Wystąpił błąd {exc.ToString()}");
             }
 
             Console.WriteLine("Koniec!");
