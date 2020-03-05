f62343f6256f1bc81fa7607b362ba92b638eba8c
s18589 <s18589@pjwstk.edu.pl>
2020-03-05 11:10:34 +0100

    catching emails

--- cw1/cw1/Program.cs
+++ cw1/cw1/Program.cs
@@ -1,16 +1,30 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace cw1
 {
-    class Program
+    public class Program
     {
-        static async Task Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var httpClient = new HttpClient();
-            var response = await httpClient.GetAsync(args[0]);
+            var response = await httpClient.GetAsync("https://www.pja.edu.pl");
 
+            if (response.IsSuccessStatusCode)
+            {
+                string html = await response.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z.]+");
+                var matches = regex.Matches(html);
+
+                foreach(var m in matches)
+                {
+                    Console.WriteLine(m);
+                }
+            }
+
+            Console.WriteLine("Koniec!");
         }
     }
 }
